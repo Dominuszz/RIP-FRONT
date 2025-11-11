5c3f3a916291380e4915ed211b3b7c370b84bdf9
Dominuszz <127459753+Dominuszz@users.noreply.github.com>
2025-11-11 16:04:58 +0300

    Header changes

--- lab5/frontend/src/components/Header/Header.tsx
+++ lab5/frontend/src/components/Header/Header.tsx
@@ -1,12 +1,20 @@
-import './Header.css'
-import logo from "../../assets/logo.png"
+import './Header.css';
+import logo from "../../assets/logo.png";
+import { ROUTES } from "../../Routes";
+import { Link } from "react-router-dom";
 
 export default function Header() {
     return (
         <header>
-            <nav className = "navbar">
-                <a href = "/"><img src = {logo} alt = "logo" /></a>
+            <nav className="navbar">
+                <div className="nav-left">
+                    <Link to={ROUTES.HOME} className="nav-logo">
+                        <img src={logo} alt="logo" className="header-logo" />
+                    </Link>
+                    <Link to={ROUTES.HOME} className="nav-title">BigOCalc</Link>
+                    <Link to={ROUTES.ComplexClasses} className="nav-title">Классы сложности</Link>
+                </div>
             </nav>
         </header>
-    )
+    );
 }
